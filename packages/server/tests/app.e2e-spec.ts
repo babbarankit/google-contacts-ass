aa739adb84317264bae597dd9500c04dfe965992
Ankit Babbar <babbar.ankit@gmail.com>
2020-09-09 00:45:30 +0530

    E2E test for server
    Signed-off-by: Ankit Babbar <babbar.ankit@gmail.com>

---
+++ packages/server/tests/app.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { NestApplication } from '@nestjs/core';
+
+describe('AppController (e2e)', () => {
+  let app: NestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    await app.init();
+  });
+
+  it('/ (GET)', done => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(404)
+      .end(done);
+  });
+
+  it('/health (GET)', done => {
+    return request(app.getHttpServer())
+      .get('/health')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect({ status: 'live' })
+      .end(done);
+  });
+
+  afterAll(async () => {
+    return await app.close();
+  }, 1000);
+});
